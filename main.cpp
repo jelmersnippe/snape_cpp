4275e47997f4ac7c2bde5e24df64d9d028bb4f62
Jelmer Snippe <jelmersnippe@hotmail.com>
2026-05-03 12:32:09 +0200

    Add clang format and tidy configs

--- main.cpp
+++ main.cpp
@@ -4,19 +4,24 @@
 #include <ctime>
 #include <deque>
 
-enum Direction { Up, Right, Down, Left };
+enum Direction {
+    Up,
+    Right,
+    Down,
+    Left
+};
 
 struct GridPoint {
-  int x, y;
+    int x, y;
 };
 
 struct Segment {
-  GridPoint position;
+    GridPoint position;
 };
 
 struct Snake {
-  Direction direction;
-  std::deque<Segment> segments;
+    Direction direction;
+    std::deque<Segment> segments;
 };
 
 const int TICKS_PER_SECOND = 4;
@@ -28,195 +33,184 @@ const int TILES_IN_WIDTH = SCREEN_WIDTH / TILE_SIZE;
 const int TILES_IN_HEIGHT = SCREEN_HEIGHT / TILE_SIZE;
 
 struct GameState {
-  double timeSinceLastTick = 0;
-  Direction desiredDirection = Up;
-  Snake snake;
-  GridPoint applePosition;
-  bool gameOver;
+    double timeSinceLastTick = 0;
+    Direction desiredDirection = Up;
+    Snake snake;
+    GridPoint applePosition;
+    bool gameOver;
 };
 
 Snake CreateSnake(int length) {
-  Snake snake;
-  snake.direction = Up;
-  snake.segments = std::deque<Segment>();
+    Snake snake;
+    snake.direction = Up;
+    snake.segments = std::deque<Segment>();
 
-  for (int i = 0; i < length; i++) {
-    Segment segment;
-    const int x = TILES_IN_WIDTH / 2;
-    const int y = (TILES_IN_HEIGHT / 2) + i;
-    segment.position.x = x;
-    segment.position.y = y;
-    snake.segments.push_back(segment);
-  }
-
-  return snake;
-}
-
-void DrawSnake(const Snake &snake) {
-  for (const Segment &segment : snake.segments) {
-    DrawRectangle(segment.position.x * TILE_SIZE,
-                  segment.position.y * TILE_SIZE, TILE_SIZE, TILE_SIZE, GREEN);
-  }
-}
-
-void DrawApple(const GridPoint &position) {
-  DrawRectangle(position.x * TILE_SIZE, position.y * TILE_SIZE, TILE_SIZE,
-                TILE_SIZE, RED);
-}
-
-GridPoint GetApplePosition(GameState &state) {
-  int x = std::rand() % TILES_IN_WIDTH;
-  int y = std::rand() % TILES_IN_HEIGHT;
-
-  while (true) {
-    if (!std::any_of(state.snake.segments.begin(), state.snake.segments.end(),
-                     [&](const Segment &s) {
-                       return s.position.x == x && s.position.y == y;
-                     })) {
-      break;
+    for (int i = 0; i < length; i++) {
+        Segment segment;
+        const int x = TILES_IN_WIDTH / 2;
+        const int y = (TILES_IN_HEIGHT / 2) + i;
+        segment.position.x = x;
+        segment.position.y = y;
+        snake.segments.push_back(segment);
     }
 
-    x = std::rand() % TILES_IN_WIDTH;
-    y = std::rand() % TILES_IN_HEIGHT;
-  }
-
-  GridPoint point;
-  point.x = x;
-  point.y = y;
-  return point;
+    return snake;
 }
 
-GridPoint GetDirection(const Direction &direction) {
-  switch (direction) {
-  case Up:
-    return {0, -1};
-  case Down:
-    return {0, 1};
-  case Right:
-    return {1, 0};
-  case Left:
-    return {-1, 0};
-  default:
-    return {0, 0};
-  }
-}
-
-bool isSameAxis(const Direction &direction, const Direction &otherDirection) {
-  return ((direction == Up || direction == Down) &&
-          (otherDirection == Up || otherDirection == Down)) ||
-         ((direction == Left || direction == Right) &&
-          (otherDirection == Left || otherDirection == Right));
-}
-
-void UpdateSnake(GameState &state) {
-  Direction decidedDirection =
-      isSameAxis(state.snake.direction, state.desiredDirection)
-          ? state.snake.direction
-          : state.desiredDirection;
-
-  const GridPoint direction = GetDirection(decidedDirection);
-  state.snake.direction = decidedDirection;
-
-  const Segment &head = state.snake.segments.front();
-
-  GridPoint newPosition = {head.position.x + direction.x,
-                           head.position.y + direction.y};
-
-  // Screen wrapping x
-  if (newPosition.x < 0) {
-    newPosition.x = TILES_IN_WIDTH - 1;
-  } else if (newPosition.x >= TILES_IN_WIDTH) {
-    newPosition.x = 0;
-  }
-
-  // Screen wrapping y
-  if (newPosition.y < 0) {
-    newPosition.y = TILES_IN_HEIGHT - 1;
-  } else if (newPosition.y >= TILES_IN_HEIGHT) {
-    newPosition.y = 0;
-  }
-
-  Segment newHead;
-  newHead.position.x = newPosition.x;
-  newHead.position.y = newPosition.y;
-
-  state.snake.segments.push_front(newHead);
-
-  if (newPosition.x == state.applePosition.x &&
-      newPosition.y == state.applePosition.y) {
-    state.applePosition = GetApplePosition(state);
-  } else {
-    state.snake.segments.pop_back();
-  }
-
-  for (int i = 1; i < state.snake.segments.size(); i++) {
-    const Segment &s = state.snake.segments[i];
-
-    if (s.position.x == newPosition.x && s.position.y == newPosition.y) {
-      state.gameOver = true;
-      break;
+void DrawSnake(const Snake& snake) {
+    for (const Segment& segment : snake.segments) {
+        DrawRectangle(segment.position.x * TILE_SIZE, segment.position.y * TILE_SIZE, TILE_SIZE, TILE_SIZE, GREEN);
     }
-  }
 }
 
-void Update(GameState &state) {
-  state.timeSinceLastTick += GetFrameTime();
-
-  if (state.timeSinceLastTick >= TICK_TIME) {
-    UpdateSnake(state);
-
-    state.timeSinceLastTick -= TICK_TIME;
-  }
+void DrawApple(const GridPoint& position) {
+    DrawRectangle(position.x * TILE_SIZE, position.y * TILE_SIZE, TILE_SIZE, TILE_SIZE, RED);
 }
 
-void HandleInput(GameState &state) {
-  if (IsKeyPressed(KEY_UP)) {
-    state.desiredDirection = Up;
-  } else if (IsKeyPressed(KEY_DOWN)) {
-    state.desiredDirection = Down;
-  } else if (IsKeyPressed(KEY_RIGHT)) {
-    state.desiredDirection = Right;
-  } else if (IsKeyPressed(KEY_LEFT)) {
-    state.desiredDirection = Left;
-  }
+GridPoint GetApplePosition(GameState& state) {
+    int x = std::rand() % TILES_IN_WIDTH;
+    int y = std::rand() % TILES_IN_HEIGHT;
+
+    while (true) {
+        if (!std::any_of(state.snake.segments.begin(), state.snake.segments.end(),
+                         [&](const Segment& s) { return s.position.x == x && s.position.y == y; })) {
+            break;
+        }
+
+        x = std::rand() % TILES_IN_WIDTH;
+        y = std::rand() % TILES_IN_HEIGHT;
+    }
+
+    GridPoint point;
+    point.x = x;
+    point.y = y;
+    return point;
 }
 
-void Draw(const GameState &state) {
-  BeginDrawing();
+GridPoint GetDirection(const Direction& direction) {
+    switch (direction) {
+    case Up:
+        return {0, -1};
+    case Down:
+        return {0, 1};
+    case Right:
+        return {1, 0};
+    case Left:
+        return {-1, 0};
+    default:
+        return {0, 0};
+    }
+}
 
-  ClearBackground(LIGHTGRAY);
+bool isSameAxis(const Direction& direction, const Direction& otherDirection) {
+    return ((direction == Up || direction == Down) && (otherDirection == Up || otherDirection == Down)) ||
+           ((direction == Left || direction == Right) && (otherDirection == Left || otherDirection == Right));
+}
 
-  DrawApple(state.applePosition);
-  DrawSnake(state.snake);
+void UpdateSnake(GameState& state) {
+    Direction decidedDirection =
+        isSameAxis(state.snake.direction, state.desiredDirection) ? state.snake.direction : state.desiredDirection;
 
-  EndDrawing();
+    const GridPoint direction = GetDirection(decidedDirection);
+    state.snake.direction = decidedDirection;
+
+    const Segment& head = state.snake.segments.front();
+
+    GridPoint newPosition = {head.position.x + direction.x, head.position.y + direction.y};
+
+    // Screen wrapping x
+    if (newPosition.x < 0) {
+        newPosition.x = TILES_IN_WIDTH - 1;
+    } else if (newPosition.x >= TILES_IN_WIDTH) {
+        newPosition.x = 0;
+    }
+
+    // Screen wrapping y
+    if (newPosition.y < 0) {
+        newPosition.y = TILES_IN_HEIGHT - 1;
+    } else if (newPosition.y >= TILES_IN_HEIGHT) {
+        newPosition.y = 0;
+    }
+
+    Segment newHead;
+    newHead.position.x = newPosition.x;
+    newHead.position.y = newPosition.y;
+
+    state.snake.segments.push_front(newHead);
+
+    if (newPosition.x == state.applePosition.x && newPosition.y == state.applePosition.y) {
+        state.applePosition = GetApplePosition(state);
+    } else {
+        state.snake.segments.pop_back();
+    }
+
+    for (int i = 1; i < state.snake.segments.size(); i++) {
+        const Segment& s = state.snake.segments[i];
+
+        if (s.position.x == newPosition.x && s.position.y == newPosition.y) {
+            state.gameOver = true;
+            break;
+        }
+    }
+}
+
+void Update(GameState& state) {
+    state.timeSinceLastTick += GetFrameTime();
+
+    if (state.timeSinceLastTick >= TICK_TIME) {
+        UpdateSnake(state);
+
+        state.timeSinceLastTick -= TICK_TIME;
+    }
+}
+
+void HandleInput(GameState& state) {
+    if (IsKeyPressed(KEY_UP)) {
+        state.desiredDirection = Up;
+    } else if (IsKeyPressed(KEY_DOWN)) {
+        state.desiredDirection = Down;
+    } else if (IsKeyPressed(KEY_RIGHT)) {
+        state.desiredDirection = Right;
+    } else if (IsKeyPressed(KEY_LEFT)) {
+        state.desiredDirection = Left;
+    }
+}
+
+void Draw(const GameState& state) {
+    BeginDrawing();
+
+    ClearBackground(LIGHTGRAY);
+
+    DrawApple(state.applePosition);
+    DrawSnake(state.snake);
+
+    EndDrawing();
 }
 
 int main(void) {
-  printf("");
-  std::srand(static_cast<unsigned>(time(nullptr)));
+    std::srand(static_cast<unsigned>(time(nullptr)));
 
-  GameState state;
-  state.snake = CreateSnake(3);
-  state.desiredDirection = Up;
-  state.timeSinceLastTick = 0;
-  state.gameOver = false;
-  GridPoint initialApplePosition = GetApplePosition(state);
-  state.applePosition = initialApplePosition;
+    GameState state;
+    state.snake = CreateSnake(3);
+    state.desiredDirection = Up;
+    state.timeSinceLastTick = 0;
+    state.gameOver = false;
+    GridPoint initialApplePosition = GetApplePosition(state);
+    state.applePosition = initialApplePosition;
 
-  InitWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "Snek");
+    InitWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "Snek");
 
-  SetTargetFPS(60);
+    SetTargetFPS(60);
 
-  while (!WindowShouldClose() && !state.gameOver) {
-    HandleInput(state);
+    while (!WindowShouldClose() && !state.gameOver) {
+        HandleInput(state);
 
-    Update(state);
+        Update(state);
 
-    Draw(state);
-  }
+        Draw(state);
+    }
 
-  CloseWindow();
+    CloseWindow();
 
-  return 0;
+    return 0;
 }
